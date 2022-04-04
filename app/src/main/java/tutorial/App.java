a40e8581d1b9737150461550b8e460b4fbafbabf
Kunal Bhatnagar <kubha99@kth.se>
2022-04-04 15:55:02 +0200

    t4

--- app/src/main/java/tutorial/App.java
+++ app/src/main/java/tutorial/App.java
@@ -5,7 +5,7 @@ package tutorial;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World!, test4";
     }
 
     public static void main(String[] args) {
